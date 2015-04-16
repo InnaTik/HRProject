e7ad16feb519ef960adc0431e57cf4a5452945c0
olga-jane <olga.jane.vakhromova@gmail.com>
2015-04-16 11:15:28 +0300

    HR main label

--- src/MainHRProgram/MainHRForm.Designer.cs
+++ src/MainHRProgram/MainHRForm.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.HRMainLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // HRMainLabel
+            // 
+            this.HRMainLabel.AutoSize = true;
+            this.HRMainLabel.Location = new System.Drawing.Point(21, 15);
+            this.HRMainLabel.Name = "HRMainLabel";
+            this.HRMainLabel.Size = new System.Drawing.Size(141, 13);
+            this.HRMainLabel.TabIndex = 0;
+            this.HRMainLabel.Text = "HR: vacancies and resumes";
             // 
             // MainHRForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.HRMainLabel);
             this.Name = "MainHRForm";
             this.Text = "HR";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label HRMainLabel;
     }
 }
 
